8a70f4e1861ff92413afb00662122fec50e4d3c7
technicalpits <technical@powerit.my>
2024-03-13 06:34:05 +0800

    first commit
    first commit

---
+++ BulkyWeb/Controllers/CustomerReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CustomerReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
